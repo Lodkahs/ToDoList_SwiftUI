509a7ebdb761e976663565aebeb2c3ac604c538f
Lodkahs <omalojiu@gmail.com>
2023-03-29 23:20:06 +0300

    Initial Commit

---
+++ ToDoList_Swift_UI/ToDoList_Swift_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList_Swift_UIApp.swift
+//  ToDoList_Swift_UI
+//
+//  Created by Andrew  on 3/29/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList_Swift_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
